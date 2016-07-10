2eb3f5f8dc2a22c0710c0811c4314f8fdad25187
mozamimy (Moza USANE) <mozamimy@quellencode.org>
2016-07-10 12:45:22 +0900

    Implement uploading file logic

--- src/lambbit_client.jsx
+++ src/lambbit_client.jsx
@@ -1,1 +1,24 @@
-console.log(process.argv);
+import AWS from 'aws-sdk';
+import FileSystem from 'fs';
+import Path from 'path';
+import UUID from 'node-uuid';
+
+const bucketName = process.argv[2];
+const filepath = process.argv[3];
+const ext = Path.extname(filepath);
+
+const s3Bucket = new AWS.S3({ params: { Bucket: bucketName } });
+const body = FileSystem.readFileSync(filepath);
+
+const params = {
+  Key: UUID.v4() + ext,
+  Body: body
+};
+
+s3Bucket.putObject(params, function(err, data) {
+  if (err) {
+    console.log('error: ', err);
+  } else {
+    console.log('success: ', data);
+  }
+});
