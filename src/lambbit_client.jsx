c19a40a15b0e02af7b61a2a7a22900091afa54c5
mozamimy (Moza USANE) <mozamimy@quellencode.org>
2016-07-16 15:10:57 +0900

    Add some options

--- src/lambbit_client.jsx
+++ src/lambbit_client.jsx
@@ -5,22 +5,33 @@ import UUID from 'node-uuid';
 import Commander from 'commander';
 
 Commander.version('0.1.0')
-  .option('-b, --bucket [name]', 'Bucket name')
-  .option('-f, --file [path]', 'File path to upload to S3')
-  .option('-e, --expire [time]', 'Expire time (seconds)')
-  .option('-m, --mail [address]', 'Mail address that you want to send')
+  .option('-b, --bucket <name>', 'bucket name')
+  .option('-f, --file <path>', 'bile path to upload to S3')
+  .option('-e, --expire <time>', 'expire time (seconds)')
+  .option('-t, --to <address>', 'to address')
+  .option('-r, --from <address>', 'from address')
+  .option('-s, --subject <string>', 'mail subject')
+  .option('-o, --body <string>', 'mail body')
   .parse(process.argv);
 
 const s3Bucket = new AWS.S3({ params: { Bucket: Commander.bucket } });
 const key = UUID.v4() + Path.extname(Commander.file);
-const body = FileSystem.readFileSync(Commander.file);
+const binary = FileSystem.readFileSync(Commander.file);
+const expire = Commander.expire;
+const toAddress = Commander.to;
+const fromAddress = Commander.from;
+const mailSubject = Commander.subject;
+const mailBody = Commander.body;
 
 const params = {
   Key: key,
-  Body: body,
+  Body: binary,
   Metadata: {
-    email: Commander.mail,
-    expires: Commander.expire
+    to: toAddress,
+    from: fromAddress,
+    subject: mailSubject,
+    body: mailBody,
+    expires: expire
   }
 };
 
